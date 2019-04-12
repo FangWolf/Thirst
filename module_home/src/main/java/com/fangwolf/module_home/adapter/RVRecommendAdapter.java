909b614e5f3909109fa4a088b986577c6a5e214f
FangWolf <siyu2510@163.com>
2019-04-12 16:46:51 +0800

    首页推荐相关icon资源

--- module_home/src/main/java/com/fangwolf/module_home/adapter/RVRecommendAdapter.java
+++ module_home/src/main/java/com/fangwolf/module_home/adapter/RVRecommendAdapter.java
@@ -25,24 +25,32 @@ public class RVRecommendAdapter extends BaseQuickAdapter<RecommendBean, BaseView
         helper.setText(R.id.tv_title, item.getTitle());
         switch (item.getCategoryId()) {
             case "44":
+                helper.setImageResource(R.id.iv_logo, R.mipmap.img_video_android);
                 break;
             case "55":
+                helper.setImageResource(R.id.iv_logo, R.mipmap.img_video_java);
                 break;
             case "66":
+                helper.setImageResource(R.id.iv_logo, R.mipmap.img_video_flutter);
                 break;
             default:
                 break;
         }
         switch (item.getRecommendStart()) {
             case 1:
+                helper.setImageResource(R.id.iv_star, R.mipmap.img_stars_1);
                 break;
             case 2:
+                helper.setImageResource(R.id.iv_star, R.mipmap.img_stars_2);
                 break;
             case 3:
+                helper.setImageResource(R.id.iv_star, R.mipmap.img_stars_3);
                 break;
             case 4:
+                helper.setImageResource(R.id.iv_star, R.mipmap.img_stars_4);
                 break;
             case 5:
+                helper.setImageResource(R.id.iv_star, R.mipmap.img_stars_5);
                 break;
         }
     }
